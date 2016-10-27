86993b1ff16f0c4ecf9a46bd791de6d7ecda976a
adahFalcon <aymen.dahech5@gmail.com>
2016-10-27 11:28:52 +0200

    add maven support

--- src/utbm/tx52/atoms_visualiser/IASphere.java
+++
@@ -1,9 +0,0 @@
-package utbm.tx52.atoms_visualiser;
-
-public interface IASphere {
-    void setRayon(double r);
-
-    void setCouleurs(double[] couleurs_);
-
-    void setPosition(double[] _position);
-}
